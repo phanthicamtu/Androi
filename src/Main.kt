e70350086e719b45606570dc3b9a2bd7c8b4a5c9
Tu <tu@Tons-Mac-Air.local>
2024-01-16 17:19:52 +0700

    Khai bao bien

---
+++ src/Main.kt
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    //1: khai bao bien
+    // Từ khoá khai báo : var,val
+    // var co the thay doi gia tri(mutable)
+    //var name:String = "Cam Tu"
+    //println(name)
+    // val ko dc gan gtri (immutable)
+    val phone : String ="093234523"
+    // Basic type : Kieu du lieu co ban
+    // Reference type : Kieu du lieu vung nho
+    var age : Int = 29
+    var height : Float = 1.75f
+    var weight : Double = 70.5
+    // boolean kieu dung sai la co true hoac false
+    var isMale : Boolean = true
+    var name = "Cam Tu"
+}
