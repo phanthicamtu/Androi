35feeca1192988efcb13810327c1de810dfaa4ca
Tu <tu@Tons-Mac-Air.local>
2024-01-17 18:49:47 +0700

    Khai bao

--- src/Main.kt
+++ src/Main.kt
@@ -1,19 +1,47 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    //1: khai bao bien
-    // Từ khoá khai báo : var,val
-    // var co the thay doi gia tri(mutable)
-    //var name:String = "Cam Tu"
-    //println(name)
-    // val ko dc gan gtri (immutable)
-    // val phone : String ="093234523"
-    // Basic type : Kieu du lieu co ban
-    // Reference type : Kieu du lieu vung nho
-    // var age : Int = 29
-    // var height : Float = 1.75f
-    // var weight : Double = 70.5
-    // boolean kieu dung sai la co true hoac false
-    // var isMale : Boolean = true
+    /*
+    1: khai bao bien
+     Từ khoá khai báo : var,val
+     var co the thay doi gia tri(mutable)
+     var name:String = "Cam Tu"
+      println(name)
+     val ko dc gan gtri (immutable)
+     val phone : String ="093234523"
+     Basic type : Kieu du lieu co ban
+     Reference type : Kieu du lieu vung nho
+     var age : Int = 29
+     var height : Float = 1.75f
+     var weight : Double = 70.5
+     boolean kieu dung sai la co true hoac false
+     var isMale : Boolean = true
+
+     3. Cau dieu kien
+     Bieu thuc so sanh :
+     lon hon : a > b
+     nho hon : a < b
+     lon hon bang : a >= b ( a>b || a == b)
+     nho hon bang : a <= b  ( a<b || a == b) => 1 trong 2 dung => true
+     4. Toan tu logic
+     1. &&  thoa tat ca dieu kien moi tra ve true
+     2. ||  tat ca dieu kien ve false se tra ve true
+     3. !   phu dinh cua dieu kien ( la dieu kien nguoc lai cua no )
+
 
 }
+
+     */
+    val a = 5
+    val b = 10
+    if(a>b){
+        print("a lon b")
+    }else if(a<b){
+        print("a nho hon b")
+    }else{
+        print(" a bang b ")
+    }
+    println("Tiep tuc")
+
+
+    }
